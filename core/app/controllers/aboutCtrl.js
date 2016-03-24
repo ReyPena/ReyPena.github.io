8ec78b7511176280e2740f87055cfbcee20bd43d
ReyPena <zonickblack@gmail.com>
2016-03-24 08:21:13 -0600

    Fix animation about and change footer style.

--- core/app/controllers/aboutCtrl.js
+++ core/app/controllers/aboutCtrl.js
@@ -1,16 +1,14 @@
-angular.module("portfolio").controller("aboutCtrl", function ($scope, $timeout, aboutService) {
+angular.module("portfolio").controller("aboutCtrl", function($scope, $timeout, aboutService) {
 
   $scope.myTechs = aboutService.mytechs;
-
-  $scope.$timeout = $timeout;
 
   $scope.show = true;
 
   var result = document.getElementsByClassName("ng-enter");
 
-  $scope.cardAction = function (index) {
+  $scope.cardAction = function(index) {
     $scope.show = !$scope.show;
-    return $scope.myTechs[index].showMe = !$scope.show;
+    $scope.myTechs[index].showMe = !$scope.show;
   };
 
 });
