f00d05fc76c247803863c37a8253e9e61caf4a6a
ReyPena <zonickblack@gmail.com>
2016-03-24 20:51:46 -0600

    Fix tooltips

--- core/app/directives/footerDirective.js
+++ core/app/directives/footerDirective.js
@@ -3,8 +3,6 @@ angular.module("portfolio").directive("footerDirective", function() {
     restrict: "E",
     templateUrl: "../views/directivesViews/footer.html",
     controller: function($scope, $window) {
-      // window.location = 'file.doc';
-      console.log($window.open);
       $scope.file = "./docs/resume.docx";
       $scope.download = function () {
         $window.open($scope.file);
