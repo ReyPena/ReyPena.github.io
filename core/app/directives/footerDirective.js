13252db8362282321c2a21e979d6e69c1f4587f2
ReyPena <zonickblack@gmail.com>
2016-03-30 20:54:38 -0600

    Clean the code.

--- core/app/directives/footerDirective.js
+++ core/app/directives/footerDirective.js
@@ -3,7 +3,7 @@ angular.module("portfolio").directive("footerDirective", function() {
     restrict: "E",
     templateUrl: "../views/directivesViews/footer.html",
     controller: function($scope, $window) {
-      $scope.file = "./docs/resume.docx";
+      $scope.file = "./docs/resume.pdf";
       $scope.download = function () {
         $window.open($scope.file);
       };
